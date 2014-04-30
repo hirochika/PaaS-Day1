1cccad8c4f63a334fe78f41dc461aeaa2a2118d2
Hirochika Asai <asai@jar.jp>
2014-04-30 13:07:04 +0900

    Added scripts used in repository.

---
+++ repository/create_repository.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+## Working repository
+WORKING_REPOSITORY=/home/ubuntu/gitolite-admin
+
+## Arguments
+repository=$1
+username=$2
+
+## Only one process is allowed
+[ -f /tmp/create_repository.lock ] && exit 1
+
+## Lock
+touch /tmp/create_repository.lock
+trap 'rm -f /tmp/create_repository.lock' 1 2
+
+## Check the argument
+if [ "$repository" = "" ];
+then
+	printf "%s <repository> <username>\n" $0
+	echo "Error: Please specify repository"
+	rm -f /tmp/create_repository.lock
+	exit 1
+fi
+if [ "$username" = "" ];
+then
+	printf "%s <repository> <username>\n" $0
+	echo "Error: Please specify username"
+	rm -f /tmp/create_repository.lock
+	exit 1
+fi
+
+cd $WORKING_REPOSITORY
+if [ ! -f keydir/$username.pub ];
+then
+	echo "Error: User not found."
+	exit 1
+fi
+echo "
+repo    $username/$repository
+        RW+     =   admin
+        RW      =   $username
+        R       =   instance" >> conf/gitolite.conf
+git commit -m "Add $repository." -a
+git push
+
+sudo sh -c "echo \"#!/bin/sh
+
+## Username/Repository
+REPOSITORY=\\\"$username/$repository\\\"
+\" > /home/git/repositories/$username/$repository.git/hooks/post-update"
+sudo sh -c "cat ~/rails_git_post_update_hook.sh >> /home/git/repositories/$username/$repository.git/hooks/post-update"
+sudo chown git /home/git/repositories/$username/$repository.git/hooks/post-update
+sudo chmod +x /home/git/repositories/$username/$repository.git/hooks/post-update
+
+## Unlock
+rm -f /tmp/create_repository.lock
+
---
+++ repository/create_user.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+## Working repository
+WORKING_REPOSITORY=/home/ubuntu/gitolite-admin
+
+## Arguments
+username=$1
+ssh_pub_key=$2
+
+## Only one process is allowed
+[ -f /tmp/create_user.lock ] && exit 1
+
+## Lock
+touch /tmp/create_user.lock
+trap 'rm -f /tmp/create_user.lock' 1 2
+
+## Check the argument
+if [ "$username" = "" ];
+then
+	printf "%s <username> <ssh_public_key>\n" $0
+	echo "Error: Please specify username"
+	rm -f /tmp/create_user.lock
+	exit 1
+fi
+if [ "$ssh_pub_key" = "" ];
+then
+	printf "%s <username> <ssh_public_key>\n" $0
+	echo "Error: Please specify SSH public key"
+	rm -f /tmp/create_user.lock
+	exit 1
+fi
+
+cd $WORKING_REPOSITORY
+if [ -f keydir/$username.pub ];
+then
+	echo "Error: User already exists."
+	exit 1
+fi
+echo "$ssh_public_key" > keydir/$username.pub
+git add keydir/$username.pub
+git commit -m "Add $username."
+git push
+
+## Unlock
+rm -f /tmp/create_user.lock
