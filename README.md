becd2f1f588038c5651ac93666aa214e2078ac9d
Hirochika Asai <asai@jar.jp>
2014-05-01 18:11:54 +0900

    Added description to README.md.

---
+++ README.md
@@ -0,0 +1,13 @@
+# PaaS (B2) Day 1
+This project is for a PBL lecture course (B2) about cloud computing at the University of Tokyo.
+
+## Source code list
+
+* instances: a set of scripts placed to the LXC container template
+** rdeploy.sh
+** rinstance.sh
+* repository: a set of scripts placed to the repository
+** rails_git_post_update_hook.sh
+** create_user.sh
+** create_repository.sh
+
--- instances/rdeploy.sh
+++ instances/rdeploy.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-kill `cat ri.pid`
-rm -rf myapp
-git clone ssh://git@your-repository-server/repository/name
+##
+## This script is NOT a good example!!
+##     e.g., containing hard coded variables
+##
 
-cd myapp
+## Kill the running instance
+kill `cat ri.pid`
+
+## Remove the old software
+rm -rf yourapp
+
+## Clone the up-to-date software
+git clone ssh://git@your-repository-server/repository/yourapp
+
+## Execute bundle install and DB migration
+cd yourapp
 export RAILS_ENV="production"
 export MYAPP_DATABASE_PASSWORD="<password>"
 bundle install --path vendor/bundle
